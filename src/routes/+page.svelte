557f31ae677c503a1f38d4704dcefab54aa85d0c
woodendoors7 <60623935+woodendoors7@users.noreply.github.com>
2023-10-15 18:40:39 +0200

    repaired WIP title message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
 
-<h1>Nuclear Option WIKI</h1>
-<p>Completely WIP.</p>
+<div style="margin-top: 120px; margin-left: 270px;">
+    <h1>Nuclear Option WIKI</h1>
+    <p>Completely WIP. Coming soon, message if you wanna contribute.</p>
+    
+</div>
