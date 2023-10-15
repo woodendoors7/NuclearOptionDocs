a63eb1192fcdcf7c6f8218072f7cf06cf55d4ead
woodendoors7 <60623935+woodendoors7@users.noreply.github.com>
2023-10-15 20:06:12 +0200

    Changed homepage, added buy on steam button, added join discord button, added youtube video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,28 @@
     <title>Nuclear Option Community WIKI</title>
 </head>
 
-<div style="margin-top: 120px; margin-left: 270px;">
+<div class="article">
     <h1>Nuclear Option Community WIKI</h1>
     <p>Nuclear Option is a combat flight game set on a dynamic and realistic battlefield of the near future. Use an array of advanced weapons to change the course of battles as you intercept bombers, duel fighters, fly under the radar, or unleash the fury of nuclear weapons on enemy formations.
        Each aircraft comes with an immersive cockpit featuring a situational awareness HUD, identifying nearby targets and threats. Aircraft can be flown with various controllers or with a mouse-controlled "virtual joystick", requiring no special hardware.</p>
     <p><b>Completely WIP.</b> Coming soon, message if you wanna contribute.</p>
+    <h3><a href="https://store.steampowered.com/app/2168680/Nuclear_Option/" target="_blank">Buy on Steam</a></h3>
+    <h3><a href="https://discord.gg/nuclear-option-909034158205059082/" target="_blank">Join Discord</a></h3>
+
+
+    <iframe id="theFrame" width="560" height="315" src="https://www.youtube-nocookie.com/embed/1vEf2TagnXc?si=PhAlk6GlZGia2rtt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     
 </div>
+
+<style>
+    #theFrame{ 
+        max-width: 90%;
+    }
+
+    .article {
+        margin: 0 auto 3em;
+        max-width: 50rem;
+        padding: 2rem;
+        margin-top: 120px;
+    }
+</style>
