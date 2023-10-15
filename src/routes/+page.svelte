babc95768c28aea519be689cf89b0e7c151d5448
woodendoors7 <60623935+woodendoors7@users.noreply.github.com>
2023-10-15 19:53:38 +0200

    changed homepage, added homepage title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 
+<head>
+    <title>Nuclear Option Community WIKI</title>
+</head>
+
 <div style="margin-top: 120px; margin-left: 270px;">
-    <h1>Nuclear Option WIKI</h1>
-    <p>Completely WIP. Coming soon, message if you wanna contribute.</p>
+    <h1>Nuclear Option Community WIKI</h1>
+    <p>Nuclear Option is a combat flight game set on a dynamic and realistic battlefield of the near future. Use an array of advanced weapons to change the course of battles as you intercept bombers, duel fighters, fly under the radar, or unleash the fury of nuclear weapons on enemy formations.
+       Each aircraft comes with an immersive cockpit featuring a situational awareness HUD, identifying nearby targets and threats. Aircraft can be flown with various controllers or with a mouse-controlled "virtual joystick", requiring no special hardware.</p>
+    <p><b>Completely WIP.</b> Coming soon, message if you wanna contribute.</p>
     
 </div>
