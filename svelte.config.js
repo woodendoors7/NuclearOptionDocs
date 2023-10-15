7444ae7faaa696cbd6aca2a65378a5cf1acac4ea
woodendoors7 <60623935+woodendoors7@users.noreply.github.com>
2023-10-15 18:25:06 +0200

    Hopefully repaired slug prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender:{
+			entries: ['/w/AGM-48', "/w/FS-12", "/w/TA-30"]
+		}
 	}
 };
 
